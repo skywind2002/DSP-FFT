445bf26f2f0374886f9bdc959bcafff328acdb42
skywind2002 <wuhc19@mails.tsinghua.edu.cn>
2021-10-18 15:55:15 +0800

    重载四则运算，乘方，模长与辐角

--- complex.cpp
+++ complex.cpp
@@ -1,3 +1,4 @@
+#define PI 3.1415926545
 #include <iostream>
 #include <math.h>
 using namespace std;
@@ -11,14 +12,109 @@ private:
 public:
   complex() : r(0), i(0) {}
   complex(double r, double i) : r(r), i(i) {}
-  complex(complex &c) : r(c.r), i(c.i) {}
+  complex(const complex &c) : r(c.r), i(c.i) {}
   ~complex() {}
 
-  friend ostream &operator<<(ostream &output, complex &c);
-  friend istream &operator>>(ostream &input, complex &c);
+  //运算符重载
+  complex &operator=(const complex &c); //赋值
+  friend const complex operator+(const complex &c1, const complex &c2);
+  friend const complex operator-(const complex &c1, const complex &c2);
+  friend const complex operator*(const complex &c1, const complex &c2);
+  friend const complex operator/(const complex &c1, const complex &c2);
+  const complex operator^(const int x); //四则运算与乘方
+
+  complex operator+=(const complex &c);
+  complex operator-=(const complex &c);
+  complex operator*=(const complex &c);
+  complex operator/=(const complex &c);
+  complex operator^=(const int x); //带等于号的四则运算与乘方
+
+  friend ostream &operator<<(ostream &output, const complex &c);
+  friend istream &operator>>(ostream &input, complex &c); //重构输入输出
+
+  double mod()
+  {
+    return sqrt(r * r + i * i);
+  }
+  double angle()
+  {
+    return atan2(i, r);
+  } //模长与辐角，辐角是弧度制
 };
 
-ostream &operator<<(ostream &output, complex &c)
+complex &complex::operator=(const complex &c)
+{
+  r = c.r;
+  i = c.i;
+  return *this;
+}
+
+const complex operator+(const complex &c1, const complex &c2)
+{
+  return complex(c1.r + c2.r, c1.i + c2.i);
+}
+
+const complex operator-(const complex &c1, const complex &c2)
+{
+  return complex(c1.r - c2.r, c1.i - c2.i);
+}
+
+const complex operator*(const complex &c1, const complex &c2)
+{
+  return complex(c1.r * c2.r - c1.i * c2.i, c1.i * c2.r + c1.r * c2.i);
+}
+
+complex complex::operator+=(const complex &c)
+{
+  complex t = *this + c;
+  *this = t;
+  return *this;
+}
+
+complex complex::operator-=(const complex &c)
+{
+  complex t = *this - c;
+  *this = t;
+  return *this;
+}
+
+complex complex::operator*=(const complex &c)
+{
+  complex t = *this * c;
+  *this = t;
+  return *this;
+}
+
+complex complex::operator/=(const complex &c)
+{
+  complex t = *this / c;
+  *this = t;
+  return *this;
+}
+
+complex complex::operator^=(const int x)
+{
+  complex t = *this ^ x;
+  *this = t;
+  return *this;
+}
+
+const complex complex::operator^(const int x)
+{
+  for (int i = 0; i < x - 1; i++)
+    (*this) *= (*this);
+  return *this;
+}
+
+const complex operator/(const complex &c1, const complex &c2)
+{
+  complex t1;
+  t1 = complex(c1.r, c1.i) * complex(c2.r, -c2.i);
+  double frac = c2.r * c2.r + c2.i * c2.i;
+  return complex(t1.r / frac, t1.i / frac);
+}
+
+ostream &operator<<(ostream &output, const complex &c)
 {
   output << c.r << '+' << c.i << 'j' << endl;
   return output;
